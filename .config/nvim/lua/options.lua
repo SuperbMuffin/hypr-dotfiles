c96dbb062250ce4669ac007e4edd2dc3d8cf72e4
Elliot Tester <elliotctester1@gmail.com>
2026-04-12 14:03:43 +0200

    Fixes

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,6 +1,9 @@
 require "nvchad.options"
 
 -- add yours here!
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.writebackup = false
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
