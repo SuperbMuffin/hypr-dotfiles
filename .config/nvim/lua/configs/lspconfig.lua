599eca0880aa6b2743e494b1e26d336fa2b58a7d
Elliot Tester <elliotctester1@gmail.com>
2026-05-25 22:16:54 +0200

    bk

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -1,10 +1,27 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls", "clangd" }
+local servers = { "html", "cssls", "clangd", "basedpyright" }
 vim.lsp.enable(servers)
 
 vim.lsp.config("clangd", {
   cmd = { "clangd" },
   filetypes = { "c", "cpp", "objc", "objcpp" },
   root_markers = { "compile_commands.json", ".clangd", ".git" },
+})
+
+vim.lsp.config("basedpyright", {
+  settings = {
+    basedpyright = {
+      analysis = {
+        typeCheckingMode = "standard",
+      },
+    },
+  },
+  before_init = function(_, config)
+    -- auto-detect .venv in project root
+    local venv = vim.fn.getcwd() .. "/.venv/bin/python"
+    if vim.fn.filereadable(venv) == 1 then
+      config.settings.basedpyright.pythonPath = venv
+    end
+  end,
 })
--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -11,16 +11,22 @@ map("i", "jk", "<ESC>")
 
 map("n", "<leader>oo", "<cmd>Octo<cr>", { desc = "Octo menu" })
 
+map("n", "<leader>m", ":make<CR>", { desc = "Make" })
+vim.keymap.set("n", "<leader>re", function()
+  local rom = vim.fn.glob "build/*.gba"
+  vim.fn.jobstart { "mgba-qt", rom }
+end)
+
 map("n", "gx", function()
-    local url = vim.fn.expand("<cfile>")
-    vim.fn.jobstart({ "xdg-open", url }, { detach = true })
+  local url = vim.fn.expand "<cfile>"
+  vim.fn.jobstart({ "xdg-open", url }, { detach = true })
 end, { desc = "Open URL under cursor" })
 
 map("n", "<leader>gi", function()
-  vim.cmd("Octo issue list")
+  vim.cmd "Octo issue list"
 end, { desc = "GitHub Issues" })
 
 map("n", "<leader>gp", function()
-  vim.cmd("Octo pr list")
+  vim.cmd "Octo pr list"
 end, { desc = "GitHub PRs" })
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
