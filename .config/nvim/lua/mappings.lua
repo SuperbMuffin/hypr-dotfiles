0d77aaaceb1867419bda200d516c575a27866b9e
Elliot Tester <elliotctester1@gmail.com>
2026-05-25 22:31:24 +0200

    harpoon

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -1,27 +1,29 @@
 require "nvchad.mappings"
 
--- add yours here
-
 local map = vim.keymap.set
+local harpoon = require('harpoon')
 
+-- move by visual line
 map("n", "<Down>", "g<Down>", { desc = "Move down visual line" })
 map("n", "<Up>", "g<Up>", { desc = "Move up visual line" })
+
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+-- Harpoon
+harpoon:setup()
+
+map("n", "<leader>a", function() harpoon:list():add() end, { desc = "Harpoon add file" })
+map("n", "<leader>h", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "Harpoon toggle menu" })
+
+map("n", "<leader>1", function() harpoon:list():select(1) end, { desc = "Harpoon go to file 1" })
+map("n", "<leader>2", function() harpoon:list():select(2) end, { desc = "Harpoon go to file 2" })
+map("n", "<leader>3", function() harpoon:list():select(3) end, { desc = "Harpoon go to file 3" })
+map("n", "<leader>4", function() harpoon:list():select(4) end, { desc = "Harpoon go to file 4" })
+
+
+-- Octo
 map("n", "<leader>oo", "<cmd>Octo<cr>", { desc = "Octo menu" })
-
-map("n", "<leader>m", ":make<CR>", { desc = "Make" })
-vim.keymap.set("n", "<leader>re", function()
-  local rom = vim.fn.glob "build/*.gba"
-  vim.fn.jobstart { "mgba-qt", rom }
-end)
-
-map("n", "gx", function()
-  local url = vim.fn.expand "<cfile>"
-  vim.fn.jobstart({ "xdg-open", url }, { detach = true })
-end, { desc = "Open URL under cursor" })
-
 map("n", "<leader>gi", function()
   vim.cmd "Octo issue list"
 end, { desc = "GitHub Issues" })
@@ -29,4 +31,17 @@ end, { desc = "GitHub Issues" })
 map("n", "<leader>gp", function()
   vim.cmd "Octo pr list"
 end, { desc = "GitHub PRs" })
+
+-- dev
+map("n", "<leader>m", ":make<CR>", { desc = "Make" })
+map("n", "<leader>re", function()
+  local rom = vim.fn.glob "build/*.gba"
+  vim.fn.jobstart { "mgba-qt", rom }
+end)
+
+-- open URL under cursor
+map("n", "gx", function()
+  local url = vim.fn.expand "<cfile>"
+  vim.fn.jobstart({ "xdg-open", url }, { detach = true })
+end, { desc = "Open URL under cursor" })
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -102,4 +102,6 @@ return {
       },
     },
   },
+
+  { "ThePrimeagen/harpoon", branch = "harpoon2", dependencies = { "nvim-lua/plenary.nvim" } },
 }
